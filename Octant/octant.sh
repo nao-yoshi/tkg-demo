30b28f88a626bd16f4077ad579226fd870e94d69
nao-yoshi <61136455+nao-yoshi@users.noreply.github.com>
2020-08-16 15:01:30 +0900

    0.2

--- Octant/octant.sh
+++ Octant/octant.sh
@@ -2,4 +2,4 @@
 
 wget https://github.com/vmware-tanzu/octant/releases/download/v0.15.0/octant_0.15.0_Linux-64bit.rpm
 sudo rpm -i octant_0.15.0_Linux-64bit.rpm
-OCTANT_LISTENER_ADDR=127.0.0.1:7777 ./octant
+OCTANT_LISTENER_ADDR=127.0.0.1:7777 octant
